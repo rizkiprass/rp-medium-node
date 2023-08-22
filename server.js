7e16e1a72ce725539ca64fc4e8cb7a974ad63be6
rizkiprass <rizkiprass@gmail.com>
2023-08-22 21:44:29 +0700

    add /api

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/api", (req, res) => {
+  res.json({ users: ["userOne", "userTwo", "userThree"] });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
