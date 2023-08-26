987247226b8aeff99706b8dba9cf74b933759ef2
rizkiprass <rizkiprass@gmail.com>
2023-08-26 15:41:18 +0700

    connect db

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-const cors = require("cors"); // Import the cors package
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const db = require("./connection");
 
-app.use(cors()); // Use the cors middleware
+app.use(cors());
+app.use(bodyParser.json());
 
 const port = 8080;
 
@@ -11,16 +13,15 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/api", (req, res) => {
-  res.json({ users: ["userOne", "userTwo", "userThree"] });
-});
-
 app.get("/db", (req, res) => {
   const sql = "SELECT * FROM user";
   db.query(sql, (err, result) => {
-    console.log(result);
-
-    response(200, result, "get all data from user", res);
+    if (err) {
+      console.error("Error querying the database:", err);
+      res.status(500).json({ error: "Error querying the database" });
+    } else {
+      res.json(result);
+    }
   });
 });
 
