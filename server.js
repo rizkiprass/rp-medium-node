cb8e43749e16aa53c5a91dc340d60695b278e821
rizkiprass <rizkiprass@gmail.com>
2023-08-26 14:04:47 +0700

    add db connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors"); // Import the cors package
+const db = require("./connection");
 
 app.use(cors()); // Use the cors middleware
 
@@ -12,6 +13,15 @@ app.get("/", (req, res) => {
 
 app.get("/api", (req, res) => {
   res.json({ users: ["userOne", "userTwo", "userThree"] });
+});
+
+app.get("/db", (req, res) => {
+  const sql = "SELECT * FROM user";
+  db.query(sql, (err, result) => {
+    console.log(result);
+
+    response(200, result, "get all data from user", res);
+  });
 });
 
 app.listen(port, () => {
