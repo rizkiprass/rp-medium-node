0792a0a95fedbc9ae28691c31e6d9c630b422b7d
rizkiprass <rizkiprass@gmail.com>
2023-09-24 12:50:53 +0700

    checkpoint: get api and db

--- connection.js
+++ connection.js
@@ -1,9 +1,20 @@
-const mysql = require("mysql");
+// const mysql = require("mysql");
+
+// const db = mysql.createConnection({
+//   host: "sandbox-mysql.cnbvaslh13te.us-east-1.rds.amazonaws.com",
+//   user: "master",
+//   password: "Uk<i>Tmv[$CX}5~}wrDkKE+9A|q|",
+//   database: "mydb",
+// });
+
+// module.exports = db;
+
+const mysql = require('mysql2') //use mysql2 to run the db mysql at container local docker
 
 const db = mysql.createConnection({
-  host: "sandbox-mysql.cnbvaslh13te.us-east-1.rds.amazonaws.com",
-  user: "master",
-  password: "Uk<i>Tmv[$CX}5~}wrDkKE+9A|q|",
+  host: "localhost",
+  user: "app",
+  password: "app",
   database: "mydb",
 });
 
