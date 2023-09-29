a9cf62001ac079138bbf2ae76e0178c94e771c4a
rizkiprass <rizkiprass@gmail.com>
2023-09-29 17:01:27 +0700

    finish image

--- connection.js
+++ connection.js
@@ -2,22 +2,9 @@
 
 // const db = mysql.createConnection({
 //   host: "sandbox-mysql.cnbvaslh13te.us-east-1.rds.amazonaws.com",
-//   user: "master",
+//   user: "app",
 //   password: "Uk<i>Tmv[$CX}5~}wrDkKE+9A|q|",
-//   database: "mydb",
+//   database: "ecommerce_db",
 // });
 
 // module.exports = db;
-
-const mysql = require('mysql2') //use mysql2 to run the db mysql at container local docker
-
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "app",
-  password: "app",
-  database: "mydb",
-});
-
-module.exports = db;
-
-//////////////////////////////////////////////////////////////
--- connection2.js
+++ connection2.js
@@ -1,9 +1,21 @@
+// const mysql = require('mysql2') //use mysql2 to run the db mysql at container local docker
+
+// const db_ecommerce = mysql.createConnection({
+//     host: "localhost",
+//     user: "app",
+//     password: "app",
+//     database: "ecommerce_db",
+//   });
+//   module.exports = db_ecommerce;
+
+require('dotenv').config();
+
 const mysql = require('mysql2') //use mysql2 to run the db mysql at container local docker
 
 const db_ecommerce = mysql.createConnection({
-    host: "localhost",
+    host: process.env.DB_ENDPOINT,
     user: "app",
-    password: "app",
+    password: process.env.DB_PASS,
     database: "ecommerce_db",
   });
   module.exports = db_ecommerce;
